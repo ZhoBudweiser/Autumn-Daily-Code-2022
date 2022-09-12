117ca0f77e75fb2e0d89df9d444892bcf58ac532
Zhohfut <413732041@qq.com>
2022-09-12 08:22:12 +0800

    0912 leetcode

---
+++ 2022-09-12-SpecialArray.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int specialArray(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end(), greater<int>());
+        for (int i = 1; i <= n; ++i) {
+            if (nums[i - 1] >= i && (i == n || nums[i] < i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
