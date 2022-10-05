c601b90f7174af541643c6e5e203eb824e2292a5
Zhohfut <413732041@qq.com>
2022-10-05 21:28:52 +0800

    1004 01

--- 2022-10-04-vector5.cpp
+++ 2022-10-04-vector5.cpp
@@ -11,6 +11,10 @@ int main() {
         cin >> num;
         v.emplace_back(num);
     }
+    int x, k1, k2;
+    cin >> k1 >> x >> k2;
+    v.insert(v.begin()+k1, x);
+    v.erase(v.begin()+k2);
     for (int i = 0; i < n; i++) {
         if (i == 0) {
             cout << v[i];
